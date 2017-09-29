c451f567e052e14019b43efb83a14827528f757e
gp <gp@gpdeMac.local>
2017-09-29 16:31:48 +0800

    Initial Commit

---
+++ object-c_test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  object-c_test
+//
+//  Created by gp on 2017/9/29.
+//  Copyright © 2017年 gp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
